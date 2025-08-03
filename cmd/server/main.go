74163e8ecaaae6de359e1d10e3aa138db71a70f3
ashborn3 <114130969+ashborn3@users.noreply.github.com>
2025-08-03 04:54:20 +0000

    Add initial server implementation with /ping endpoint and dependencies

---
+++ cmd/server/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	r := chi.NewRouter()
+
+	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"resp":"pong"}`))
+	})
+
+	fmt.Println("Bundlr server running on port", port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
